e1a66ac954b694fd2fb98c182394f3e5a0db5c71
thorff1 <thorff1@gmail.com>
2015-10-27 06:49:02 -0400

    Tests
    See slack for error info. Have re-written the passing tests for Emailer
    and UserDao. Last night I had tests written for every method in all
    classes except base.java, github windows git ide fucked me over. I had
    written each test initially to fail as per QA testing guidelines and
    then changed it to an attempt to pass. (Which to for-freaking-ever). I
    will re-write the tests for the DTOs and work on modularizing our ugly
    base.java over the next 2 days. so that on thursday I can actually
    finish testing base.java

--- osgtesting/src/osgtesting/dao/userDaoTest.java
+++ osgtesting/src/osgtesting/dao/userDaoTest.java
@@ -2,15 +2,28 @@ package osgtesting.dao;
 
 import static org.junit.Assert.*;
 import java.sql.*;
-
+import java.util.List;
+import java.util.ArrayList;
 import org.junit.Test;
-
 import osgtesting.Model.UserDTO;
 
 public class userDaoTest {
 
+	/*******************/
+	/** Creation Test **/
+	/*******************/
 	@Test
-	public void testLogin() throws SQLException {
+	public void testUserDaoCreation() {
+		userDao tester = new userDao();
+		
+		assertNotNull( tester );
+	}
+	
+	/****************/
+	/** Login Test **/
+	/****************/
+	@Test
+	public void testUserDaoLogin() throws SQLException {
 		userDao tester = new userDao();
 		ResultSet rs = tester.login("test", "test123");
 		Statement st;
@@ -19,8 +32,11 @@ public class userDaoTest {
 				st.toString(), "Select username,password,salt from freesurfer_interface.users Where username='test'");
 	}
 
+	/***************/
+	/** Edit Test **/
+	/***************/
 	@Test
-	public void testEdit() throws SQLException {
+	public void testUserDaoEdit() throws SQLException {
 		userDao tester = new userDao();
 		ResultSet rs = tester.edit("test2");
 		Statement st;
@@ -28,12 +44,65 @@ public class userDaoTest {
 		assertEquals("Statement in rs is what is expected",
 				st.toString(), "Select * from freesurfer_interface.users where username='test2'");
 	}
-
+	
+	/***************/
+	/** Read Test **/
+	/***************/
+	@Test
+	public void testUserDaoRead() {
+		userDao tester = new userDao();
+		List<UserDTO> users = tester.read();
+		
+		assertNotNull( users );
+	}
+	
+	/****************************************************************/
+	/** Write Test                                                 **/
+	/*  To test this functionality remove "try {" and the entirety  */
+	/*  of the "catch" block from the function and uncomment the    */
+	/*  throw                                                       */
+	/****************************************************************/
 	@Test(timeout=500)
-	public void testWrite() {
+	public void testUserDaoWrite() {
 		userDao tester = new userDao();
 		UserDTO account = new UserDTO("test", "test123", "testfirst", "testlast", "test@test.com", "tester", "1231231234", "abcd");
-		tester.Write(account);	
+		try {
+			tester.Write(account);	
+		} catch ( Exception e ) {
+			fail();
+		}
 	}
-
+	
+	/****************************************************************/
+	/** Update Account Test                                        **/
+	/*  To test this functionality remove "try {" and the entirety  */
+	/*  of the "catch" block from the function and uncomment the    */
+	/*  throw                                                       */
+	/****************************************************************/
+	@Test(timeout=500)
+	public void testUserDaoUpdateAccount() {
+		userDao tester = new userDao();
+		UserDTO account = new UserDTO("test", "test123", "testfirst", "testlast", "test@test.com", "tester", "1231231234", "abcd");
+		try {
+			tester.update(account);	
+		} catch ( Exception e ) {
+			fail();
+		}
+	}
+	
+	/****************************************************************/
+	/** Update Password Test                                       **/
+	/*  To test this functionality remove "try {" and the entirety  */
+	/*  of the "catch" block from the function and uncomment the    */
+	/*  throw                                                       */
+	/****************************************************************/
+	@Test(timeout=500)
+	public void testUserDaoUpdatePassword() {
+		userDao tester = new userDao();
+		try {
+			tester.updatePassword( "test", "test123", "abcd" );	
+		} catch ( Exception e ) {
+			fail();
+		}
+	}
 }
