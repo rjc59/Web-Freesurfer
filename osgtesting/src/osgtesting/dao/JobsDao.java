65c2875431aa47040320b17d243b0963c27388ab
[WMcKibbin] <[williams.mckibbin@gmail.com]>
2015-11-09 21:17:32 -0500

    Fixed rest call to use query string parameters

--- osgtesting/src/osgtesting/dao/JobsDao.java
+++ osgtesting/src/osgtesting/dao/JobsDao.java
@@ -9,44 +9,44 @@ import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+import com.squareup.okhttp.HttpUrl;
 public class JobsDao {
-	private Connection con;
-	private String dbURL;
-	private String osg_interface_url;
+	private String freesurfer_interface;
+	private int port;
 	private OkHttpClient client;
 	private CryptoToolbox hasher;
 	private String timeStamp;
 	private String token;
 	public JobsDao(UserDTO user){
-		dbURL = "jdbc:postgresql://webfreesurferdb.cbiow68bwd0c.us-east-1.rds.amazonaws.com:5432/osgtestdb";
 		client = new OkHttpClient();
 		hasher = new CryptoToolbox();
+		//Get Unix Epoch time
 		timeStamp = Long.toString((System.currentTimeMillis() / 1000L));
-		
-		byte[] tokenBytes = hasher.hashSHA256(user.getSalt().concat(timeStamp).getBytes());
-		token = new String(tokenBytes);
-		try{
-			DriverManager.registerDriver(new org.postgresql.Driver());
-			con = DriverManager.getConnection(dbURL,"administrator","osgtestdatabase");
-			System.out.println(con.equals(null));
-		}
-		catch(SQLException e){
-			e.printStackTrace();
-			System.err.println("Could not access "+ dbURL);
-		}
+		//token = SHA256 of Salt + User's Password + Epoch time
+		byte[] token_bytes = hasher.hashSHA256(user.getSalt().concat(user.getPass()).concat(timeStamp).getBytes());
+		token = new String(token_bytes);
+		freesurfer_interface = "localhost";
+		port = 8081;
 	}
 	public void Write(JobsDTO job){
 		//create request body
 		RequestBody file_body = RequestBody.create(MediaType.parse("application/plain"), job.getJobFile());
 		RequestBody request_body = new MultipartBuilder().type(MultipartBuilder.FORM)
-				.addFormDataPart("userid", job.getAuthor().getId())
-				.addFormDataPart("token", token)
-				.addFormDataPart("singlecore", "1")
-				.addFormDataPart("jobname", job.getJobFile().getName())
 				.addFormDataPart("jobfile", job.getJobFile().getName(), file_body)
 				.build();
+		//create url
+		HttpUrl request_url = new HttpUrl.Builder()
+				.scheme("http")
+				.host(freesurfer_interface)
+				.port(port)
+				.addPathSegment("freesurfer")
+				.addPathSegment("jobs")
+				.addQueryParameter("userid", job.getAuthor().getId())
+				.addQueryParameter("token", token)
+				.addQueryParameter("singlecore", "true")
+				.addQueryParameter("jobname", job.getJobFile().getName())
+				.build();
 		//create post request
-		String request_url = osg_interface_url+ "/freesurfer/jobs";
 		Request http_request = new Request.Builder()
 			.url(request_url)
 			.post(request_body)
@@ -55,11 +55,26 @@ public class JobsDao {
 		Response http_response;
 		try{
 			http_response = client.newCall(http_request).execute();	
+			switch(http_response.code())
+			{
+				case 200:
+					System.err.println("Jobfile:"+job.getJobFile().getName()+" was successfully submitted.");
+					break;
+				case 400:
+					System.err.println("Job submission for file:"+ job.getJobFile().getName() + " had malformed parameters");
+					break;
+				case 500:
+					System.err.println("Job submission for file:"+ job.getJobFile().getName() + " caused a server error");
+					break;
+			}
+			//clean up jobfile
 		}
 		catch(Exception e)
 		{
+			//clean up job file
 			e.printStackTrace();
 		}
+		
 	}
 }
 
